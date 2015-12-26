6d46d83d186d23a99e50a979b33431f375bde3a1
Toby Flemming <tobyf93@hotmail.com>
2015-12-27 09:18:07 +1100

    Fetching playlists that the user owns

--- moodify/app/controllers/pages_controller.rb
+++ moodify/app/controllers/pages_controller.rb
@@ -1,7 +1,27 @@
 class PagesController < ApplicationController
   def index
+
+    if request.env['omniauth.auth'].present?
+      user = RSpotify::User.new(request.env['omniauth.auth'])
+      # Now you can access user's private data, create playlists and much more
+
+      playlists = user.playlists
+      @my_playlists = get_my_playlists user.id, playlists
+      # @tracks = playlists.first.tracks
+
+    end
+
   end
 
-  def login
+  def get_my_playlists user_id, playlists
+    my_playlists = []
+
+    playlists.map do |playlist|
+      if playlist.owner.id == user_id
+        my_playlists.push playlist
+      end
+    end
+
+    my_playlists
   end
 end
--- moodify/config/routes.rb
+++ moodify/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root :to => 'pages#index'
 
   get 'index' => 'pages#index'
+  get '/auth/spotify/callback' => 'pages#index'
   get 'login' => 'pages#login'
 end
