d713de31a7f56a14999fb5c0bb1c26bc56c661f7
Toby Flemming <tobyf93@hotmail.com>
2015-12-26 14:34:03 +1100

    Spotify OAuth working

--- moodify/config/initializers/omniauth.rb
+++ moodify/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "<your_client_id>", "<your_client_secret>", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, '75ae9a46e92b4925a74619d8eb8e1556', '2c556c74e45347b09bbda2879a0b7a63', scope: 'playlist-read-private playlist-modify-public user-library-read'
 end
