b92c6d1a4acfb2741239c286a96b8101fa727d2b
Toby Flemming <tobyf93@hotmail.com>
2016-05-17 21:35:52 +1000

    SpotifyConnector used to handle third-party Spotify library.  Also simplifies calls made in apiServer.js by implementing custom methods.

--- apiServer.js
+++ apiServer.js
@@ -2,7 +2,7 @@ var express = require('express'),
     serveStatic = require('serve-static'),
     SpotifyWebApi = require('spotify-web-api-node'),
     cookieParser = require('cookie-parser'),
-    spotifyConnector = require('./lib/spotifyConnector');
+    SpotifyConnector = require('./lib/SpotifyConnector');
 
 module.exports = function(PORT) {
   var app = express();
@@ -14,19 +14,20 @@ module.exports = function(PORT) {
 
 
   app.get('/login', function(req, res) {
-    var spotifyApi = spotifyConnector();
-    var scopes = ['user-read-private', 'user-read-email'];
-    var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+    var config = {
+      scopes: ['user-read-private', 'user-read-email']
+    };
+
+    var spotifyConnector = new SpotifyConnector(config);
+    var authorizeURL = spotifyConnector.createAuthorizeURL();
     res.redirect(authorizeURL);
   });
 
-  // Use authCode to retreive accessToken.  Store token in cookie for use in
-  // API calls.
   app.get('/callback', function(req, res) {
-    var authCode = req.query.code;
+    var spotifyConnector = new SpotifyConnector(req.query);
 
-    spotifyConnector()
-      .authenticate(authCode)
+    spotifyConnector
+      .authenticate()
       .then(function(data) {
         res.cookie('accessToken', data.accessToken);
         res.cookie('refreshToken', data.refreshToken);
@@ -39,13 +40,15 @@ module.exports = function(PORT) {
   });
 
   app.get('/playlists', function(req, res) {
-    var spotifyApi = spotifyConnector(req.cookies);
+    var spotifyConnector = new SpotifyConnector(req.cookies);
 
-    spotifyApi.getUserPlaylists()
+    spotifyConnector
+      .getUserPlaylists()
       .then(function(data) {
-        console.log('Retrieved playlists', data.body);
-      },function(err) {
-        console.log('Something went wrong!', err);
+        res.send(data);
+      },
+      function(err) {
+        res.send(err);
       });
   });
 
